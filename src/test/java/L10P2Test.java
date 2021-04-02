fc56eb92dd349cabd00fa1cbff50b6a3cf4a6702
Pcow9 <71295830+Pcow9@users.noreply.github.com>
2021-04-01 21:06:35 -0400

    Add so search bar gets text

--- src/test/java/L10P2Test.java
+++ src/test/java/L10P2Test.java
@@ -22,34 +22,156 @@ import org.openqa.selenium.Keys;
 import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 public class L10P2Test {
     private WebDriver driver;
     private Map<String, Object> vars;
     JavascriptExecutor js;
     @Before
     public void setUp() {
-        System.setProperty("webdriver.gecko.driver", "/Users/miablo/Downloads/geckodriver");
-        //System.setProperty("webdriver.chrome.driver", "/Users/miablo/Downloads/chromedriver");
-        driver = new FirefoxDriver();
-        //driver = new ChromeDriver();
+       // System.setProperty("webdriver.gecko.driver", "c:/temp/geckodriver");
+       // driver = new FirefoxDriver();
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
+        System.setProperty("webdriver.chrome.driver", "C:/temp/chromedriver.exe");
+        driver = new ChromeDriver();
     }
     @After
     public void tearDown() {
         driver.quit();
     }
     @Test
-    public void l10P2() {
+    public void l10P2() throws InterruptedException {
         driver.get("https://docs.oracle.com/javase/9/docs/api/overview-summary.html");
-        driver.manage().window().setSize(new Dimension(680, 761));
-        driver.findElement(By.id("search")).sendKeys("java.lang.string");
-        driver.findElement(By.id("search")).sendKeys(Keys.ENTER);
+        driver.manage().window().setSize(new Dimension(680, 759));
+        driver.findElement(By.id("search")).sendKeys("java.lang.StrictMath");
+        TimeUnit.SECONDS.sleep(2);
+        driver.findElement(By.id("search")).click();
+        {
+            WebElement element = driver.findElement(By.linkText("java.lang.StrictMath"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        driver.findElement(By.cssSelector("#ui-id-7255 .resultHighlight")).click();
         driver.findElement(By.cssSelector(".rowColor:nth-child(3) > .colConstructorName a")).click();
         driver.findElement(By.id("search")).click();
-        driver.findElement(By.id("search")).sendKeys("javax.swing.jframe");
-        driver.findElement(By.id("search")).sendKeys(Keys.DOWN);
-        driver.findElement(By.id("search")).sendKeys(Keys.ENTER);
+        {
+            WebElement element = driver.findElement(By.cssSelector("#ui-id-9761 .resultHighlight"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#ui-id-9762 .resultHighlight"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#ui-id-9762 .resultHighlight"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#ui-id-9762 .resultHighlight"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("div:nth-child(6) li:nth-child(2) > a"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#ui-id-9761 .resultHighlight"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#ui-id-9762 .resultHighlight"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.linkText("javax.swing.JFrame.AccessibleJFrame"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#ui-id-9767 .resultHighlight"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#ui-id-9767 .resultHighlight"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#allclasses_navbar_top a"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#ui-id-9768 .resultHighlight"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#ui-id-9767 .resultHighlight"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        driver.findElement(By.cssSelector("#ui-id-9768 .resultHighlight")).click();
         driver.findElement(By.cssSelector(".altColor:nth-child(4) > .colConstructorName .memberNameLink > a")).click();
     }
 }
